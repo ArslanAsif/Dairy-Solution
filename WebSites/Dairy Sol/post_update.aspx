4d50600d35b889e829167745fecff45aa8c8e962
khalidmansoor42 <khalidmansoor42@yahoo.com>
2015-06-22 15:07:26 +0500

    updated post_update page

---
+++ WebSites/Dairy Sol/post_update.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage2.master" enableEventValidation="false" AutoEventWireup="true" CodeFile="post_update.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Add Post <small>Updates</small>
+                </h1>
+            </div>
+        </div>
+				
+        <form class="form-group">
+            <div class="row">
+                <div class="col-lg-12">
+
+                <div class="col-md-3 col-md-offset-1">
+                    <br /><br/>
+                    <asp:Image ID="Image1" runat="server" width="250px" Height="250px"/>
+                    <br/>
+                    <label>Attach Picture</label><asp:FileUpload ID="postImage" runat="server" />
+                    <asp:Button runat="server" cssClass="btn btn-xs btn-primary" OnClick="changePic" Text="Upload"/>
+                </div>
+                <div class="col-md-6 col-md-offset-1">
+                    <br />
+                    <asp:Button runat="server" CssClass="btn btn-sm btn-primary pull-right" Text="Update Date" OnClick="changeDate"></asp:Button>
+                    <asp:Label cssClass="pull-right" ID="date" runat="server" Text="Label" style="margin-right:10px"></asp:Label><br/><br/>
+                    <asp:TextBox ID="postHeading" runat="server" type="text" class="form-control control-height" placeholder="Heading"></asp:TextBox>
+					<br/>
+					<textarea class="form-control" id="postSummary" runat="server" name="suppName" placeholder="Short Summary"/><br />
+                    <textarea class="form-control" rows="10" id="postDetail" runat="server" name="suppName" placeholder="Detail"/><br />
+                    
+                    <br/>
+                    <asp:Button ID="updatePost" runat="server" Text="Update Post" style = "width: 100%; margin-left: 2px" type = "submit" class = "btn btn-primary" OnClick="updatePost_Click"/>
+                    <br/>
+                </div>
+                </div>
+            </div>
+            <br />
+        </form>					
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+</asp:Content>
---
+++ WebSites/Dairy Sol/post_update.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!Page.IsPostBack)
+        {
+            retrieve_data();
+        }
+    }
+
+    protected void retrieve_data( ) 
+    { 
+        string post_id = Request.QueryString["id"];
+        string query = "SELECT * from posts where post_id = '"+ post_id +"'";
+        string constring = ConfigurationManager.ConnectionStrings["Dairy_SolutionConnectionString"].ConnectionString;
+        SqlConnection con = new SqlConnection(constring);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = query;
+        
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            if( dr.HasRows )
+            {
+                date.Text = dr["date"].ToString();
+                Image1.ImageUrl = dr["picture"].ToString();
+                postHeading.Text = dr["heading"].ToString();
+                postSummary.InnerText = dr["summary"].ToString();
+                postDetail.InnerText = dr["detail"].ToString();  
+            }
+        }// while loop
+
+        con.Close();
+    }
+
+    protected void updatePost_Click(object sender, EventArgs e) 
+    {
+        if (postImage.HasFile)
+        {
+            string extension = System.IO.Path.GetExtension(postImage.FileName);
+            if (extension.ToLower() != ".gif" && extension.ToLower() != ".png" && extension.ToLower() != ".jpg" && extension.ToLower() != ".jpeg")
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "popup", "alert('Invalid Image Format!');", true);
+            }
+            else
+            {
+                string pathName = "images/" + Path.GetFileName(postImage.PostedFile.FileName);
+                postImage.SaveAs(Server.MapPath("~/images/updates/" + postImage.FileName));
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "popup", "alert('File Uploaded!');", true);
+            }
+        }
+        string post_id = Request.QueryString["id"];
+        string constring = ConfigurationManager.ConnectionStrings["Dairy_SolutionConnectionString"].ConnectionString;
+        string query = "UPDATE posts SET heading=@heading, summary=@summary, detail=@detail, picture=@picture, date=@date WHERE post_id = '" + post_id + "'";
+        SqlConnection con = new SqlConnection(constring);
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = con;
+        cmd.CommandText = query;
+        
+        cmd.Parameters.AddWithValue("@heading", postHeading.Text);
+        cmd.Parameters.AddWithValue("@summary", postSummary.InnerText);
+        cmd.Parameters.AddWithValue("@detail", postDetail.InnerText);
+        cmd.Parameters.AddWithValue("@date",date.Text);
+
+        if (postImage.PostedFile.FileName != "")
+        {
+            cmd.Parameters.AddWithValue("@picture", "images/updates/" + postImage.PostedFile.FileName.ToString());
+        }
+        else 
+        {
+            cmd.Parameters.AddWithValue("@picture", Image1.ImageUrl);
+        }
+
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Response.Redirect("post_manage.aspx");
+    }
+
+    protected void changeDate(object sender, EventArgs e) 
+    {
+        date.Text = Convert.ToString(DateTime.Now);
+    }
+    protected void changePic(object sender, EventArgs e)
+    {
+        if (postImage.HasFile)
+        {
+            Image1.ImageUrl = "images/updates/" + postImage.PostedFile.FileName.ToString();
+        }
+    }
+}
